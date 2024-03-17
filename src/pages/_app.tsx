1f35fd4c6f7afa7b3f8f0699cfceb2e92cf70604
elskow <elskow@outlook.com>
2024-03-17 23:07:18 +0700

    feat : refactor code architecture, implement button usage, better dropdown component, remove user defined color, etc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,14 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import Navbar from "./component/navbar";
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
     return (
         <div className={` ${inter.className} `}>
-            <Navbar />
-            <div className="mx-auto min-w-3.5 max-w-screen-xl">
-                <Component {...pageProps} />
-            </div>
+            <Component {...pageProps} />
         </div>
     );
 }
