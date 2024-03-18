e6bf08f42554c77e5dda3b3c25f5f7fa2bb80073
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-03-18 09:25:23 +0700

    Revert "feat: type of button"
    This reverts commit 922ed4a448aa9fbf15f5cfbcc1cbcfca6b9712f5.

--- src/pages/component/text-area-output.tsx
+++ src/pages/component/text-area-output.tsx
@@ -1,19 +1,19 @@
-import { FaCopy, FaDownload } from "react-icons/fa";
+import {FaCopy, FaDownload} from "react-icons/fa";
 import React from "react";
 
-interface TextAreaOutputProps {
-    textOutput: string;
-}
+export function TextAreaOutput({...params}) {
+    const textOutput = params.textOutput ? params.textOutput : "";
 
-export function TextAreaOutput({ textOutput }: TextAreaOutputProps) {
-    function copyToClipboard() {
-        navigator.clipboard.writeText(textOutput);
-        alert("Copied to clipboard");
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(textOutput)
+            .then(
+                _ => alert("Copied to clipboard")
+            );
     }
 
-    function downloadText() {
+    const downloadText = () => {
         const element = document.createElement("a");
-        const file = new Blob([textOutput], { type: "text/plain" });
+        const file = new Blob([textOutput], {type: 'text/plain'});
         element.href = URL.createObjectURL(file);
         element.download = "output.txt";
         document.body.appendChild(element);
@@ -21,12 +21,12 @@ export function TextAreaOutput({ textOutput }: TextAreaOutputProps) {
     }
 
     return (
-        <div className="p-5 w-full flex flex-col">
+        <div className="w-full flex flex-col">
             <div className="rounded-md">
                 <div className="flex flex-col">
                     <label
                         htmlFor="output"
-                        className="text-gray-800 text-opacity-80 bg-gray-200 rounded-t-md py-2 pl-4 font-semibold"
+                        className="text-gray-800 text-opacity-80 bg-gray-200 rounded-t-md py-2 pl-4 font-semibold select-none"
                     >
                         Teks Parafrase
                     </label>
@@ -35,30 +35,29 @@ export function TextAreaOutput({ textOutput }: TextAreaOutputProps) {
                         name="output"
                         id=""
                         rows={15}
-                        className="text-gray-800 py-4 pl-4 outline-none w-full"
+                        className="text-gray-600 py-4 pl-4 outline-none w-full text-area-scrollbar pr-2"
                         readOnly
                         value={textOutput}
-                        style={{ resize: "none" }}
+                        style={{resize: "none"}}
+                        placeholder={"Teks hasil parafrase akan muncul disini..."}
                     ></textarea>
                 </div>
                 <div className="flex bg-gray-200 p-2 rounded-b-md justify-end w-full pr-4">
                     <div className="flex gap-4">
                         <button
-                            type="button"
                             onClick={copyToClipboard}
                             title="copyButton"
                             className="bg-white p-2 rounded-lg text-gray-700 hover:bg-sky-100 transition duration-200 ease-in-out"
                         >
-                            <FaCopy />
+                            <FaCopy/>
                         </button>
 
                         <button
-                            type="button"
                             onClick={downloadText}
                             title="downloadButton"
                             className="bg-white p-2 rounded-lg text-gray-700 hover:bg-sky-100 transition duration-200 ease-in-out"
                         >
-                            <FaDownload />
+                            <FaDownload/>
                         </button>
                     </div>
                 </div>
