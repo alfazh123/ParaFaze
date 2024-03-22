ef820421a9294c9aab0cb7d69c772ad40d6d52b8
elskow <elskow@outlook.com>
2024-03-22 07:23:00 +0700

    BREAKING CHANGES: use paraphraser model (dedicated) based on t5

---
+++ server/src/paraphraser/language.py
@@ -0,0 +1,7 @@
+LANGUAGE_LIST : dict = {
+    "english" : "Vamsi/T5_Paraphrase_Paws",
+    "german" : "Lelon/t5-german-paraphraser-small",
+    "indonesia" : "Wikidepia/IndoT5-base-paraphrase",
+    "malaysia" : "mesolitica/finetune-paraphrase-t5-base-standard-bahasa-cased",
+    "south_korea" : "lcw99/t5-base-korean-paraphrase"
+}
